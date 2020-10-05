e39a9b9d405c1240204f38bded7b8f94ac1a16ab
aaronangxz <angxze@gmail.com>
2020-10-05 20:04:20 +0800

    Updated Practice in C

---
+++ Practice in C/00."Hello World!"_in_C.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+    //"%[^\n]%*c" -> to read the whole line in scanf
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
